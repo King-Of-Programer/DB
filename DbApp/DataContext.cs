b3555ebef63f5082e7c3bade0e957a7dbc3f9c16
King-Of-Programer <alexdichemix@gmail.com>
2024-03-12 15:13:20 +0200

    init

---
+++ DbApp/DataContext.cs
@@ -0,0 +1,45 @@
+﻿using Firebase.Database;
+using Firebase.Database.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbApp
+{
+    public class DataContext
+    {
+        private FirebaseClient firebase;
+        public DataContext(string databaseUrl)
+        {
+            firebase = new FirebaseClient(databaseUrl);
+        }
+        public async Task<List<Worker>> GetAllWorkers()
+        {
+            var students = await firebase
+            .Child("Workers")
+            .OnceAsync<Worker>();
+
+            return students.Select(item => new Worker
+            {
+                Id = item.Object.Id,
+                FirstName = item.Object.FirstName,
+                LastName = item.Object.LastName
+            }).ToList();
+
+        }
+
+        public async Task AddPerson(Worker stud)
+        {
+            await firebase
+                .Child("Workers")
+                .PostAsync(new Worker()
+                {
+                    Id = Guid.NewGuid(),
+                    FirstName = stud.FirstName,
+                    LastName = stud.LastName,
+                });
+        }
+    }
+}
---
+++ DbApp/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DbApp
+{
+    public partial class MainPage : ContentPage
+    {
+        private DataContext _dataContext;
+        public MainPage()
+        {
+            InitializeComponent();
+            _dataContext = new DataContext("https://xamapp-af217-default-rtdb.europe-west1.firebasedatabase.app/");
+        }
+        private Label CreateLabel(string text)
+        {
+            var label = new Label();
+            label.FontSize = 15;
+            label.Text = text;
+            label.TextColor = Color.Black;
+            return label;
+
+        }
+        private async void addWorker_Click(object sender, EventArgs e)
+        {
+            workersStack.Children.Clear();
+            Worker worker = new Worker()
+            {
+                FirstName = fNameEntry.Text,
+                LastName = lNameEntry.Text,
+            };
+            await _dataContext.AddPerson(worker);
+            var students = await _dataContext.GetAllWorkers();
+            foreach (var student in students)
+            {
+                var label = CreateLabel($"Id: {student.Id},First name: {student.FirstName}, Last name: {student.LastName}");
+                workersStack.Children.Add(label);
+            }
+        }
+    }
+}
